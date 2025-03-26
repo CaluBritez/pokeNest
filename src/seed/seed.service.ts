9a050e66fbcfd61d859fa747da85d0589b26ccb9
Calu <lucas_britez@hotmail.com>
2025-03-26 20:11:34 -0300

    seed lograda de manera eficiente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+      @InjectModel( Pokemon.name )
+      private readonly pokemonModel: Model<Pokemon>
+    ) {}
   async executeSeed() {
 
-    const { data }= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
+    await this.pokemonModel.deleteMany({});
 
-    data.results.forEach(async ({ name, url }) => {
-      const segments = url.split('/'); // https://pokeapi.co/api/v2/pokemon/1/
-      const no = +segments[segments.length - 2]; // id = 1
+    const { data }= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
-      console.log({ name, no });
-      
+    const pokemonsToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach( ({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      pokemonsToInsert.push({ name, no });
 
     });
 
-    return data;
+    await this.pokemonModel.insertMany( pokemonsToInsert );
+
+    /*
+    const insertPromisesArray: Promise<Pokemon>[] = [];
+
+    data.results.forEach( ({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      //const pokemon = await this.pokemonModel.create({ name, no });
+      insertPromisesArray.push(
+        this.pokemonModel.create({ name, no })
+      );
+
+    });
+    await Promise.all( insertPromisesArray );
+    */
+
+    return 'Seed Executed';
   }
 }
